66f5a0261ba77dc9421ebb6bd2e84d99dd1c16b2
enbohm <andreas@enbohms.se>
2018-09-15 16:48:58 +0200

    Added specific metrics to hello-endpoint

--- src/main/java/se/enbohms/helidon/HelloEndpoint.java
+++ src/main/java/se/enbohms/helidon/HelloEndpoint.java
@@ -9,6 +9,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.metrics.annotation.Counted;
+
 @ApplicationScoped
 @Path("/")
 public class HelloEndpoint {
@@ -16,6 +18,7 @@ public class HelloEndpoint {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/hello/{name}")
+	@Counted
 	public Response sayHi(@PathParam("name") String name) {
 		return Response.ok(Json.createObjectBuilder().add("Hello ", name).build()).build();
 	}
